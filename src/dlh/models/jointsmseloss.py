5dd4f8e36bf59eb48f80687a99de382d52dba22b
Nathan Molinier <nathan.molinier@gmail.com>
2023-09-20 11:10:38 -0400

    change default weighted loss to consider all masks

--- src/dlh/models/jointsmseloss.py
+++ src/dlh/models/jointsmseloss.py
@@ -10,19 +10,22 @@ from __future__ import division
 from __future__ import print_function
 
 import torch.nn as nn
+import torch
 
 
 class JointsMSELoss(nn.Module):
-    def __init__(self, use_target_weight=True):
+    def __init__(self, use_target_weight=False):
         super(JointsMSELoss, self).__init__()
         self.criterion = nn.MSELoss(reduction='mean')
         self.use_target_weight = use_target_weight
 
-    def forward(self, output, target, target_weight):
+    def forward(self, output, target, target_weight, vis_out):
         batch_size = output.size(0)
         num_joints = output.size(1)
         heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
         heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
+        
+        #loss = self.criterion(vis_out, target_weight) # init loss with false detections (non empty masks)
         loss = 0
 
         for idx in range(num_joints):
