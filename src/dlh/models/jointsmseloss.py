d1a13dd252ab7acb96eaeb72c48b1369f51b107f
Nathan Molinier <nathan.molinier@gmail.com>
2024-02-07 16:32:13 -0500

    Add new BCE loss

--- src/dlh/models/jointsmseloss.py
+++ src/dlh/models/jointsmseloss.py
@@ -16,7 +16,7 @@ import torch
 class JointsMSELoss(nn.Module):
     def __init__(self, use_target_weight=False):
         super(JointsMSELoss, self).__init__()
-        self.criterion = nn.MSELoss(reduction='mean')
+        self.MSEcriterion = nn.MSELoss(reduction='mean')
         self.use_target_weight = use_target_weight
 
     def forward(self, output, target, target_weight, vis_out):
@@ -25,18 +25,45 @@ class JointsMSELoss(nn.Module):
         heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
         heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
         
-        #loss = self.criterion(vis_out, target_weight) # init loss with false detections (non empty masks)
         loss = 0
 
         for idx in range(num_joints):
             heatmap_pred = heatmaps_pred[idx].squeeze()
             heatmap_gt = heatmaps_gt[idx].squeeze()
             if self.use_target_weight:
-                loss += 0.5 * self.criterion(
+                loss += 0.5 * self.MSEcriterion(
                     heatmap_pred.mul(target_weight[:, idx]),
                     heatmap_gt.mul(target_weight[:, idx])
                 )
             else:
-                loss += 0.5 * self.criterion(heatmap_pred, heatmap_gt)
+                loss += 0.5 * self.MSEcriterion(heatmap_pred, heatmap_gt)
+
+        return loss / num_joints
+
+class JointsMSEandBCELoss(nn.Module):
+    def __init__(self, use_target_weight=False):
+        super(JointsMSELoss, self).__init__()
+        self.MSEcriterion = nn.MSELoss(reduction='mean')
+        self.BCEcriterion = nn.BCELoss()
+        self.use_target_weight = use_target_weight
+
+    def forward(self, output, target, target_weight, vis_out):
+        batch_size = output.size(0)
+        num_joints = output.size(1)
+        heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
+        heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
+        
+        loss = self.BCEcriterion(vis_out, target_weight) # init loss with false detections (non empty masks)
+
+        for idx in range(num_joints):
+            heatmap_pred = heatmaps_pred[idx].squeeze()
+            heatmap_gt = heatmaps_gt[idx].squeeze()
+            if self.use_target_weight:
+                loss += 0.5 * self.MSEcriterion(
+                    heatmap_pred.mul(target_weight[:, idx]),
+                    heatmap_gt.mul(target_weight[:, idx])
+                )
+            else:
+                loss += 0.5 * self.MSEcriterion(heatmap_pred, heatmap_gt)
 
         return loss / num_joints
