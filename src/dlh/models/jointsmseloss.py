6c5c8bedef6ee1b9e4f933fae1ca2a99f4127836
Nathan Molinier <nathan.molinier@gmail.com>
2024-02-26 10:08:45 -0500

    Update loss

--- src/dlh/models/jointsmseloss.py
+++ src/dlh/models/jointsmseloss.py
@@ -51,14 +51,18 @@ class JointsMSEandBCELoss(nn.Module):
     def forward(self, output, target, target_weight):
         batch_size = output.size(0)
         num_joints = output.size(1)
-        heatmaps_pred = output.reshape((batch_size, num_joints, -1)).split(1, 1)
-        heatmaps_gt = target.reshape((batch_size, num_joints, -1)).split(1, 1)
+        heatmaps_pred = output.reshape((batch_size, num_joints, -1))
+        heatmaps_pred_tuple = heatmaps_pred.split(1, 1)
+        heatmaps_gt = target.reshape((batch_size, num_joints, -1))
+        heatmaps_gt_tuple = heatmaps_gt.split(1, 1)
         
-        loss = 0 #self.BCEcriterion(vis_out, target_weight) # init loss with false detections (non empty masks)
+        sum_pred = torch.sum(heatmaps_pred, axis=2)
+        sum_gt = torch.sum(heatmaps_gt, axis=2)
+        loss = 0 #0.00005 * self.BCEcriterion(sum_pred, sum_gt) # init loss with false detections (non empty masks) sum all the pixels in the image
 
         for idx in range(num_joints):
-            heatmap_pred = heatmaps_pred[idx].squeeze()
-            heatmap_gt = heatmaps_gt[idx].squeeze()
+            heatmap_pred = heatmaps_pred_tuple[idx].squeeze()
+            heatmap_gt = heatmaps_gt_tuple[idx].squeeze()
             if self.use_target_weight:
                 loss += 1 * self.MSEcriterion(
                     heatmap_pred.mul(target_weight[:, idx]),
@@ -71,6 +75,45 @@ class JointsMSEandBCELoss(nn.Module):
             else:
                 loss += 0.5 * self.MSEcriterion(heatmap_pred, heatmap_gt)
 
+        return loss / num_joints
+
+
+class JointsMSEandBCEandDICELoss(nn.Module):
+    def __init__(self):
+        super(JointsMSEandBCEandDICELoss, self).__init__()
+        self.MSEcriterion = nn.MSELoss(reduction='mean')
+        self.BCEcriterion = nn.BCEWithLogitsLoss()
+        self.DICEcriterion = diceloss()
+
+    def forward(self, output, target, target_weight):
+        batch_size = output.size(0)
+        num_joints = output.size(1)
+        heatmaps_pred = output.reshape((batch_size, num_joints, -1))
+        heatmaps_pred_tuple = heatmaps_pred.split(1, 1)
+        heatmaps_gt = target.reshape((batch_size, num_joints, -1))
+        heatmaps_gt_tuple = heatmaps_gt.split(1, 1)
+        
+        sum_pred = torch.sum(heatmaps_pred, axis=2)
+        sum_gt = torch.sum(heatmaps_gt, axis=2)
+        loss_mse = 0
+        loss_bce = 0
+        loss_dice = 0
+        for idx in range(num_joints):
+            heatmap_pred = heatmaps_pred_tuple[idx].squeeze()
+            heatmap_gt = heatmaps_gt_tuple[idx].squeeze()
+            loss_mse += self.MSEcriterion(
+                heatmap_pred.mul(target_weight[:, idx]),
+                heatmap_gt.mul(target_weight[:, idx])
+            )
+            loss_bce += self.BCEcriterion(
+                heatmap_pred.mul(torch.where(target_weight[:, idx]==0,1.,0)),
+                heatmap_gt.mul(torch.zeros_like(target_weight[:, idx]))
+            )
+            loss_dice += self.DICEcriterion(
+                heatmap_pred.mul(torch.where(target_weight[:, idx]==0,1.,0)),
+                heatmap_gt.mul(torch.zeros_like(target_weight[:, idx]))
+            )
+        loss = 1*loss_mse + 0.005*loss_bce + 0*loss_dice
         return loss / num_joints
 
 
@@ -97,3 +140,18 @@ class JointsBCELoss(nn.Module):
             ) 
 
         return loss / num_joints
+
+class diceloss(torch.nn.Module):
+    '''
+    Based on:
+    - https://discuss.pytorch.org/t/implementation-of-dice-loss/53552
+    - https://github.com/Project-MONAI/MONAI/blob/dev/monai/losses/dice.py 
+    '''
+    def init(self):
+        super(diceLoss, self).init()
+    def forward(self, pred, target):
+        smooth = 1e-5
+        intersection = (pred * target).sum()
+        pred_sum = torch.sum(pred)
+        target_sum = torch.sum(target)
+        return 1 - ((2. * intersection + smooth) / (pred_sum + target_sum + smooth))
